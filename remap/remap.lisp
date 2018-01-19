3765038403a484166298142acda6c203b61abc18
Michal Herda <michal.herda@ericsson.com>
2018-01-19 19:22:29 +0100

    optimize EVEN MORE

--- remap/remap.lisp
+++ remap/remap.lisp
@@ -5,28 +5,47 @@
 
 (in-package #:cl-furcadia/remap)
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar *x000000ff*
+    #.(make-array 4 :element-type '(unsigned-byte 8)
+                    :initial-contents '(0 0 0 255)))
+  (defvar *x00000000*
+    #.(make-array 4 :element-type '(unsigned-byte 8)
+                    :initial-contents '(0 0 0 0))))
+
 (defun 8bit-32bit (vector &optional remapp)
   "Converts the provided image data from 8-bit to 32-bit. If REMAPP is true, the
 resulting image will be remappable."
+  (declare (optimize speed)
+           (type (simple-array (unsigned-byte 8) (*)) vector))
   (let* ((length (array-total-size vector))
          (result (make-array (* 4 length) :element-type '(unsigned-byte 8)))
-         (palette *classic-palette*))
-    (labels ((pref (c i) (aref palette (+ i (* 4 c)))))
-      (loop for i from 0 below length
+         (palette *classic-palette*)
+         (scratch (make-array 4 :element-type '(unsigned-byte 8)
+                                :initial-contents '(0 0 0 255))))
+    (declare (type (simple-array (unsigned-byte 8) (*)) palette))
+    (labels ((pref (c i) (aref palette (+ i (* 4 c))))
+             (arr (a b c d) (setf (aref scratch 0) a (aref scratch 1) b
+                                  (aref scratch 2) c (aref scratch 3) d)
+               scratch))
+      (declare (inline arr pref))
+      (loop for i of-type (unsigned-byte 24) from 0 below length
             for color = (aref vector i)
-            for (keyword index) = (gethash color *legacy-remaps*)
+            for value = (gethash color *legacy-remaps*)
+            for keyword = (first value)
+            for index of-type (unsigned-byte 3) = (second value)
             if (and remapp keyword index)
               do (let ((g (assoc-value *legacy-remap-types* keyword)))
                    (setf (subseq result (* 4 i) (+ 4 (* 4 i)))
                          (if (eq g :shadow)
-                             #(0 0 0 0)
-                             `#(0 ,g ,(nth (- 7 index) *gradient-stops*) 255))))
+                             #.*x00000000* ;; TODO optimize this thing below
+                             (arr 0 g (nth (- 7 index) *gradient-stops*) 255))))
             else
               do (setf (subseq result (* 4 i) (+ 4 (* 4 i)))
                        (if (= color 0)
-                           #(0 0 0 0)
-                           `#(,(let ((b (pref color 2))) (if (= 0 b) 1 b))
-                              ,(pref color 1) ,(pref color 0) ,(pref color 3))))
+                           #.*x00000000*
+                           (arr (let ((b (pref color 2))) (if (= 0 b) 1 b))
+                                (pref color 1) (pref color 0) (pref color 3))))
             finally (return result)))))
 
 (defun remap-all (color-code &rest image-datas)
@@ -47,14 +66,15 @@ unknown remaps codes that were encountered."
 image data with all eligible pixels remapped. The second value returns all
 unknown remaps codes that were encountered."
   (check-type color-code (or null string))
-  (check-type image-data vector)
+  (check-type image-data (simple-array (unsigned-byte 8) (*)))
   (let* ((gradients (all-gradients color-code)))
     (%remap image-data gradients)))
 
 (defun %remap (image-data gradients)
+  (declare (optimize speed)
+           (type (simple-array (unsigned-byte 8) (*)) image-data))
   (let* ((length (length image-data))
-         (result (make-array length :element-type '(unsigned-byte 8)
-                                    :initial-contents image-data))
+         (result (copy-seq image-data))
          (missing-remaps '()))
     (loop for i from 0 below length by 4
           for b = (aref result (+ i 0))
@@ -62,24 +82,31 @@ unknown remaps codes that were encountered."
           if (and (= b 0) (/= g 0))
             do (when (and (/= g 255) (not (nth-value 1 (gethash g gradients))))
                  (pushnew g missing-remaps))
-               (setf (subseq result i (+ i 4))
-                     (remap-pixel (subseq result i (+ i 4)) gradients)))
+               (let ((remap (remap-pixel (subseq result i (+ i 4)) gradients)))
+                 (declare (type (simple-array (unsigned-byte 8) (4)) remap))
+                 (setf (subseq result i (+ i 4)) remap)))
     (values result missing-remaps)))
 
 (defun remap-pixel (pixel gradients)
   "Remaps the provided pixel using the provided gradients. GRADIENTS should be
 a hashtable in the same format as valid output from ALL-GRADIENTS."
-  (declare (optimize speed))
-  (destructuring-bind (b g r a) (coerce pixel 'list)
+  (declare (optimize speed)
+           (type (simple-array (unsigned-byte 8) (4)) pixel))
+  (let ((b (aref pixel 0)) (g (aref pixel 1))
+        (r (aref pixel 2)) (a (aref pixel 3)))
     (declare (type (unsigned-byte 8) b g r a))
-    (cond
-      ((/= b 0) pixel)
-      ((= g 255) #(0 0 0 255))
-      ((not (nth-value 1 (gethash g gradients))) pixel)
-      (t (let ((gradient (gethash g gradients)))
-           (declare (type (vector (unsigned-byte 8)) gradient))
-           (let ((color (subseq gradient (* r 4) (+ (* r 4) 4))))
-             `#(,(aref color 2) ,(aref color 1) ,(aref color 0) ,a)))))))
+    (cond ((/= b 0) pixel)
+          ((= g 255) *x000000ff*)
+          ((not (nth-value 1 (gethash g gradients))) pixel)
+          (t (let ((gradient (gethash g gradients))
+                   (result (make-array 4 :element-type '(unsigned-byte 8))))
+               (declare (type (vector (unsigned-byte 8)) gradient))
+               (let ((color (subseq gradient (* r 4) (+ (* r 4) 4))))
+                 (setf (aref result 0) (aref color 2)
+                       (aref result 1) (aref color 1)
+                       (aref result 2) (aref color 0)
+                       (aref result 3) a)
+                 result))))))
 
 (defun color-code-gradient (type code)
   "Given a color type and a color code, returns the respective gradient."
@@ -126,14 +153,13 @@ respective blends to the hash-table."
             for position below 5
             for factor of-type single-float = step then (+ factor step)
             for blend = (make-array 1024 :element-type '(unsigned-byte 8))
-            do (loop for i from 0 below 1024
-                     for x of-type (unsigned-byte 8) = (aref slide-1 i)
-                     for y of-type (unsigned-byte 8) = (aref slide-2 i)
+            do (setf (gethash (+ start position) hash-table) blend)
+               (loop for i from 0 below 1024
+                     for x across slide-1
+                     for y across slide-2
                      for lerp = (round (the (single-float 0.0 255.0)
                                             (lerp factor x y)))
-                     do (setf (aref blend i) lerp)
-                     finally (setf (gethash (+ start position) hash-table)
-                                   blend))))))
+                     do (setf (aref blend i) lerp))))))
 
 #|
 18:29 < stassats> and index-1 and index-2 is ub8, why aren't you using an
