99e001f5ade82d77b8d68c908c1974cb466d1318
Michal Herda <michal.herda@ericsson.com>
2018-01-29 14:55:38 +0100

    fix KS names

--- constants/constants.lisp
+++ constants/constants.lisp
@@ -106,7 +106,7 @@ codes. CARs are valid as partial input to *LEGACY-REMAPS*.")
   "All blends defined in the FOX5 specification, in form of (STARTING-SLIDE
 COUNT COLOR-1 COLOR-2).")
 
-(defvar *kitterspeak*
+(defparameter *kitterspeak*
   '((1 . :frame) (2 . :delay) (3 . :loop) (4 . :jump) (5 . :posx) (6 . :posy)
     (7 . :furre-x) (8 . :furre-y)
     (9 . :draw-front) ;; legacy
@@ -115,10 +115,10 @@ COUNT COLOR-1 COLOR-2).")
     (14 . :rand-frame-delay) (15 . :shape-frame)
     (16 . :shape-frame) ;; obsolete
     (17 . :opacity) (18 . :slide-posx) (19 . :slide-posy) (20 . :slide-furrex)
-    (21 . :slide-furrey) (22 . :slide-opacity) (23 . :show-bgframe)
-    (24 . :show-fgframe) (25 . :show-bgshape) (26 . :show-fgshape)
-    (27 . :hide-bg) (28 . :hide-fg) (29 . :frame-behind) (30 . :frame-front)
-    (31 . :move-forward) (32 . :move-backward))
+    (21 . :slide-furrey) (22 . :slide-opacity) (23 . :show-bg-frame)
+    (24 . :show-fg-frame) (25 . :show-bg-object) (26 . :show-fg-object)
+    (27 . :hide-bg) (28 . :hide-fg) (29 . :show-behind-frame)
+    (30 . :show-front-frame) (31 . :move-forward) (32 . :move-backward))
   "The alist of Kitterspeak step/rule codes and their names.
 \
 Rules 14+ seem to be unused in FOX1.")
